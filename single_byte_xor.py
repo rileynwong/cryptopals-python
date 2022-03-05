2dc0c1dd0343548378da0ca1f6554877bdcbabb0
Bert Muthalaly <bert@somethingdoneright.net>
2022-03-05 15:21:30 -0600

    Cookin mc's like a pound of bacon!
    bert loves riley :-)<3

--- single_byte_xor.py
+++ single_byte_xor.py
@@ -1,51 +1,121 @@
-import numpy
+from itertools import repeat
+from collections import defaultdict
+from math import inf
 
 
-def single_xor(hex_buffer_1, single_hex):
+ASCII_FREQUENCIES = {
+  ' ': 18.74,
+  'E': 9.60,
+  'T': 7.02,
+  'A': 6.21,
+  'O': 5.84,
+  'I': 5.22,
+  'N': 5.21,
+  'H': 4.87,
+  'S': 4.77,
+  'R': 4.43,
+  'D': 3.52,
+  'L': 3.20,
+  'U': 2.25,
+  'M': 1.94,
+  'C': 1.88,
+  'W': 1.82,
+  'G': 1.66,
+  'F': 1.62,
+  'Y': 1.56,
+  'P': 1.31,
+  ',': 1.24,
+  '.': 1.21,
+  'B': 1.19,
+  'K': 0.74,
+  'V': 0.71,
+  '"': 0.67,
+  '\'': 0.44,
+  '-': 0.26,
+  '?': 0.12,
+  'X': 0.12,
+  'J': 0.12,
+  ';': 0.08,
+  '!': 0.08,
+  'Q': 0.07,
+  'Z': 0.07,
+  ':': 0.03,
+  '1': 0.02,
+  '—': 0.01,
+  '0': 0.01,
+  ')': 0.01,
+  '*': 0.01,
+  '(': 0.01,
+  '2': 0.01,
+  '’': 0.01,
+  '`': 0.01,
+  '“': 0.01,
+  '”': 0.01,
+  '3': 0.01,
+  '9': 0.01,
+  '5': 0.01,
+  '4': 0.01,
+}
 
-    # hex to bytestrings
-    bytes_buffer_1 = bytes.fromhex(hex_buffer_1)
-    bytes_buffer_2 = bytes.fromhex(single_hex)
 
-    b1 = numpy.frombuffer(bytes_buffer_1, dtype='uint8')
-    b2 = numpy.frombuffer(bytes_buffer_2, dtype='uint8')
-
-    # xor bytestrings
-    xor_bytes = (b1 ^ b2)
-    print('xor_bytes: ')
-    print(xor_bytes)
-
-    # convert back to hex
-    xor_hex = xor_bytes.tobytes().hex()
-
-    return xor_hex
+# Takes two frequency dictionaries and computes a similarity score using the
+# sum of squared differences.
+def sum_of_squared_residuals(f1, f2):
+    ks = f1.keys() | f2.keys()
+    residuals = [(f1[k] - f2[k]) ** 2 for k in ks]
+    return sum(residuals)
 
 
+def byte_xor(ba1, ba2):
+    """ XOR two byte strings """
+    return bytes([a ^ b for a, b in zip(ba1, ba2)])
 
-def iterate_hex():
-    buffer_1 = '1c0111001f010100061a024b53535009181c'
 
+def character_frequencies(input):
+    input = input.lower()
+
+    # Calculate character counts
+    counts = defaultdict(lambda: 0.0)
+    for c in input:
+        counts[chr(c)] += 1
+
+    # Calculate character frequencies
+    input_len = len(input)
+    freqs = defaultdict(lambda: 0.0)
+    for k, v in counts.items():
+        freqs[k] = (v / input_len) * 100
+
+    return freqs
+
+
+def iterate_ascii():
+    buffer_1 = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+
+    best_result = None
+    best_dec = None
+    score_min = inf
     # Iterate through decimal set of possible ASCII
-    for decimal in range(0, 128):
+    for decimal in range(32, 127):
         print('trying dec: ', str(decimal))
 
         # Convert decimal to formatted hex
-        hex_val = hex(decimal)[2:].zfill(2)
-
-        print('hex_val: ', hex_val)
+        ascii_byte_repeated = bytearray(repeat(decimal, len(buffer_1)))
 
         # Try hex value as single_hex key
-        xor_hex_result = single_xor(buffer_1, hex_val)
-        print('xor_hex_result: ', xor_hex_result)
+        xor_result = byte_xor(buffer_1, ascii_byte_repeated)
+        print('xor_result: ', xor_result)
 
-        # Convert hex to ASCII
-        bytes_object = bytes.fromhex(xor_hex_result)
-        ascii_result = bytes_object.decode("ASCII")
-
-        print('ascii result: ', ascii_result)
+        score = sum_of_squared_residuals(defaultdict(lambda: 0.0, ASCII_FREQUENCIES), character_frequencies(xor_result))
+        print('score: ', score)
+        if score < score_min:
+            score_min = score
+            best_result = xor_result
+            best_dec = decimal
         print()
+
+    return (best_dec, score_min, best_result)
 
 
 if __name__ == "__main__":
 
-    iterate_hex()
+    print(iterate_ascii())
